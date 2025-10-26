f97ac04a1cd70b4af2fadc4e1aaf99192ba866b4
Anthony <106450012+AnthonyBAC@users.noreply.github.com>
2025-10-26 01:55:01 -0300

    -

--- src/main/java/bargamingBackend/bargaming/modules/products/repository/ProductRepository.java
+++ src/main/java/bargamingBackend/bargaming/modules/products/repository/ProductRepository.java
@@ -1,9 +1,8 @@
 package bargamingBackend.bargaming.modules.products.repository;
 
 import bargamingBackend.bargaming.modules.products.model.Product;
+import bargamingBackend.bargaming.modules.auth.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
