ae0d0b827216b8674545b57a03b72cce4c4ef504
Sirious4k <173212312+Sirious4k@users.noreply.github.com>
2025-10-27 17:33:03 -0300

    Cambio en SecurityConfig para productById

--- src/main/java/bargamingBackend/bargaming/modules/auth/config/SecurityConfig.java
+++ src/main/java/bargamingBackend/bargaming/modules/auth/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package bargamingBackend.bargaming.modules.auth.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -26,7 +27,8 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/register", "/api/auth/login", "/api/productos").permitAll()
+                        .requestMatchers("/api/auth/register", "/api/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/productos/**").permitAll()
                         .requestMatchers("/api/client/**").hasAuthority("CLIENTE").requestMatchers("/api/seller/**")
                         .hasAuthority("VENDEDOR").requestMatchers("/api/admin/**").hasAuthority("ADMIN").anyRequest()
                         .authenticated())
